416992ac18b86a346cfae235d494c331b5ab7135
MaisaEngineering <contact@maisasolutions.com>
2012-09-10 18:19:50 +0530

    added paypal stander payment getway

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,23 @@
 class Product < ActiveRecord::Base
   attr_accessible :name, :price
+  
+  def paypal_url(return_url)
+  	values = {
+    	:business => 'diatm._1347268590_biz@gmail.com',
+   		:cmd => '_cart',
+ 	    :upload => 1,
+	    :return => return_url,
+ 	    :invoice => id
+	  }
+	  
+  	  values.merge!({
+  	   "amount_1" => 10,
+ 	     "item_name_1" => 'test',
+ 	     "item_number_1" => 1,
+ 	     "quantity_1" => 1
+  	  })
+	  
+	  "https://www.sandbox.paypal.com/cgi-bin/webscr?" + values.to_query
+	end
+  
 end
