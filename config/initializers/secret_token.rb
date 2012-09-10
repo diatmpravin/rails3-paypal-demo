c409bfc018967db44f03aec7c2f9a813440980fb
MaisaEngineering <contact@maisasolutions.com>
2012-09-10 18:07:18 +0530

    initail commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rails3PaypalDemo::Application.config.secret_token = 'c69638416b7598a9cfc5ca8213c244090598ed655c77ad5462205657e378413fd35708a583dffaaa9b1de82b03c98366771eebc3eb1e71258450989fe10be8ef'
